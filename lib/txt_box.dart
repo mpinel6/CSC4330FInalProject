e574d1d8bb283a93079117419168fa5dd72b785e
COLBY BLANK <colbyblank@gmail.com>
2025-04-16 16:11:08 -0500

    Created base for "ChatBox"

--- lib/txt_box.dart
+++ lib/txt_box.dart
@@ -1,2 +1,20 @@
 // This file will be a chat box functionality
 // by Colby Blank
+import 'package:flutter/material.dart';
+
+class ChatBox extends StatefulWidget {
+  const ChatBox({super.key});
+  @override
+  _ChatBoxState createState() => _ChatBoxState();
+}
+
+class _ChatBoxState extends State<ChatBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(child: Text("Chat")),
+      ),
+    );
+  }
+}
