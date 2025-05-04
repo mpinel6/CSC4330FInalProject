ce4f6a778a4035b7834b8ab74490e62ce30988ec
JacobRodrigue03 <jrod171@lsu.edu>
2025-05-03 22:36:55 -0500

    Updated audiomanger.dart
    Added functions to pause, resume, and stop music. These will be implemented when pressing buttons on the home page so the music only plays when on that home page.

--- lib/audio_manager.dart
+++ lib/audio_manager.dart
@@ -12,17 +12,29 @@ class AudioManager {
   final AudioPlayer _musicPlayer = AudioPlayer();
   final AudioPlayer _fxPlayer = AudioPlayer();
 
+  double _musicVolume = 0.5;
+  double _fxVolume = 0.5;
+
   Future<void> playMusic(String path) async {
     await _musicPlayer.setReleaseMode(ReleaseMode.loop);
     await _musicPlayer.play(AssetSource(path), volume: _musicVolume);
   }
 
+  Future<void> stopMusic() async {
+    await _musicPlayer.stop();
+  }
+
+  Future<void> pauseMusic() async {
+    await _musicPlayer.pause();
+  }
+
+  Future<void> resumeMusic() async {
+    await _musicPlayer.resume();
+  }
+
   Future<void> playFx(String path) async {
     await _fxPlayer.play(AssetSource(path), volume: _fxVolume);
   }
-
-  double _musicVolume = 0.5;
-  double _fxVolume = 0.5;
 
   void setMusicVolume(double volume) {
     _musicVolume = volume;
